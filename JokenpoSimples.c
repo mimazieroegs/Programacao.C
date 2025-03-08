a767177c2ec4a6047a186e3498b50698343edb61
Michelle Maziero <micomprasmaziero@gmail.com>
2025-03-08 17:39:25 -0300

    Create JokenpoSimples.c

---
+++ JokenpoSimples.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main () {
+int escolhaJogador, escolhaComputador;
+srand(time(0));
+
+printf("***  Jogo de Jokenpô  ***\n");
+printf("Escolha uma opção:\n");
+printf("1. Pedra\n");
+printf("2. Papel\n");
+printf("3. Tesoura\n");
+printf("Escolha: ");
+scanf("%d", &escolhaJogador);
+
+escolhaComputador = rand() % 3 + 1;
+
+switch (escolhaJogador)
+{
+case 1:
+  printf("Jogador: Pedra\n");
+  break;
+  case 2:
+  printf("Jogador: Papel\n ");
+  break;
+  case 3:
+  printf("Jogador: Tesoura\n ");
+  break;
+default:
+printf ("Opção inválida.\n");
+  break;
+}
+switch (escolhaComputador)
+{
+case 1:
+  printf("Computador: Pedra\n ");
+  break;
+  case 2:
+  printf("Computador: Papel\n ");
+  break;
+  case 3:
+  printf("Computador: Tesoura\n ");
+  break;
+}
+
+  if (escolhaComputador == escolhaJogador)
+  {
+   printf(" ### O jogo empatou! ###\n");
+  }else if ((escolhaJogador == 1) && (escolhaComputador == 3)||
+             (escolhaJogador == 2) && (escolhaComputador == 1) ||
+             (escolhaJogador == 3) && (escolhaComputador == 2))
+  {
+    printf(" ###  Parbéns, você ganhou!  ###\n");
+  } else
+  {
+    printf("### Você perdeu!###\n");
+  }
+    
+return 0;
+}
